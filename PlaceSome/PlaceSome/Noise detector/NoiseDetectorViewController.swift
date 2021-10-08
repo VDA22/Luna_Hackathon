0fc85e03475992826dea4648b558b54a6f9bb033
Anton Glezman <a.glezman@opends.tech>
2021-10-09 00:24:59 +0300

    Верстка

--- PlaceSome/PlaceSome/Noise detector/NoiseDetectorViewController.swift
+++ PlaceSome/PlaceSome/Noise detector/NoiseDetectorViewController.swift
@@ -21,7 +21,6 @@ class NoiseDetectorViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        setupSession()
         configureAudioEngine()
         try? match()
     }
@@ -33,44 +32,6 @@ class NoiseDetectorViewController: UIViewController {
         audioRecorder = nil
         timer?.cancel()
     }
-
-//    @IBAction private func startRecord() {
-//        if audioRecorder == nil {
-//            setupSession()
-//        } else {
-//            audioRecorder?.stop()
-//            try? audioSession.setActive(false)
-//            audioRecorder = nil
-//            timer?.cancel()
-//        }
-//    }
-
-//    private func setupSession() {
-//        guard let url = directoryURL() else {
-//            print("Unable to find a init directoryURL")
-//            return
-//        }
-//
-//        let recordSettings = [
-//            AVSampleRateKey : NSNumber(value: Float(44100.0) as Float),
-//            AVFormatIDKey : NSNumber(value: Int32(kAudioFormatMPEG4AAC) as Int32),
-//            AVNumberOfChannelsKey : NSNumber(value: 1 as Int32),
-//            AVEncoderAudioQualityKey : NSNumber(value: Int32(AVAudioQuality.medium.rawValue) as Int32),
-//        ]
-//
-//        do {
-//            try audioSession.setCategory(AVAudioSession.Category.playAndRecord)
-//            try audioSession.setActive(true)
-//            let audioRecorder = try AVAudioRecorder(url: url, settings: recordSettings)
-//            audioRecorder.prepareToRecord()
-//            audioRecorder.isMeteringEnabled = true
-//            audioRecorder.record()
-//            recordForever(audioRecorder: audioRecorder)
-//            self.audioRecorder = audioRecorder
-//        } catch let err {
-//            print("Unable start recording", err)
-//        }
-//    }
     
     func directoryURL() -> URL? {
         let fileManager = FileManager.default
@@ -83,7 +44,7 @@ class NoiseDetectorViewController: UIViewController {
     func recordForever(audioRecorder: AVAudioRecorder) {
         let queue = DispatchQueue(label: "io.segment.decibel", attributes: .concurrent)
         timer = DispatchSource.makeTimerSource(flags: [], queue: queue)
-        timer?.schedule(deadline: .now(), repeating: 0.5, leeway: .milliseconds(100))
+        timer?.schedule(deadline: .now(), repeating: 0.4, leeway: .milliseconds(100))
         timer?.setEventHandler { [weak self] in
             audioRecorder.updateMeters()
             let average = audioRecorder.averagePower(forChannel: 0)
@@ -100,12 +61,13 @@ class NoiseDetectorViewController: UIViewController {
         print("avg: \(avgdB), peak: \(peakdB)")
         
         samples.append(peakdB)
-        if samples.count > 8 {
+        if samples.count > 12 {
             samples = Array(samples.dropFirst())
-        }
-        
-        DispatchQueue.main.async {
-            self.dbValueLabel.text = "\(peakdB)"
+            
+            DispatchQueue.main.async {
+                let movingAverage = self.samples.reduce(0, { $0 + $1 }) / Float(self.samples.count)
+                self.dbValueLabel.text = "\(movingAverage)"
+            }
         }
     }
     
